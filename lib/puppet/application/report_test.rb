a072adbedca2e18c32d4eb6978c225942c9f6c4e
James Sweeny <james.sweeny@puppetlabs.com>
2013-03-16 13:12:47 -0400

    Fix application

--- lib/puppet/application/report_test.rb
+++ lib/puppet/application/report_test.rb
@@ -1,4 +1,5 @@
 require 'puppet/application/indirection_base'
 
-class Puppet::Application::Report < Puppet::Application::IndirectionBase
+class Puppet::Application::Report_test < Puppet::Application::FaceBase
+  run_mode :master
 end
